aada6de5118b7fec926f15609db3668c1c8211cf
Romain Roux <cuisinier.sonore@gmail.com>
2019-05-21 17:43:35 +0200

    bold selected item navBar

--- src/Component/NavarBarBurger.jsx
+++ src/Component/NavarBarBurger.jsx
@@ -7,22 +7,51 @@ class NavarBarBurger extends Component {
         super(props);
         this.state = {
             options: [
-                { key: 1, text: 'Child 1', value: 1, href:"#/" },
-                { key: 2, text: 'Child 2', value: 2, href:"#/" },
-                { key: 3, text: 'Child 3', value: 3, href:"#/" },
-            ]
+                { key: 1, text: 'Child 1', value: 1, href: "#/" },
+                { key: 2, text: 'Child 2', value: 2, href: "#/" },
+                { key: 3, text: 'Child 3', value: 3, href: "#/" },
+            ],
+            activeItem: 'notreExpertise'
         }
     }
+    onClickMenu = (page) => {
+        this.setState({ activeItem: page })
+    }
     render() {
-        const { options } =this.state;
+        const { options, activeItem } = this.state;
         return (
             <div >
                 <Menu className='NavBarApp' pageWrapId={"page-wrap"}>
-                    <a id="notreExpertise" className="menu-item" href="#/">Notre Expertise</a>
-                    <a id="innovation" className="menu-item" href="#innovation">Innovation R&D</a>
-                    <a id="ingénierie" className="menu-item" href="#ingenierie">Ingénierie</a>
-                    <a id="industrialisation" className="menu-item" href="#industrialisation">Industrialisation</a>
-                    <a id="qualification" className="menu-item" href="#qualification">Qualification produit</a>
+                    <a
+                        id="notreExpertise"
+                        onClick={() => this.onClickMenu('notreExpertise')}
+                        className={`${activeItem === 'notreExpertise' ? 'menu-item navbar-Selected' : 'menu-item'}`}
+                        href="#/">Notre Expertise
+                    </a>
+                    <a
+                        id="innovation"
+                        onClick={() => this.onClickMenu('innovation')}
+                        className={`${activeItem === 'innovation' ? 'menu-item navbar-Selected' : 'menu-item'}`}
+                        href="#innovation">Innovation R&D
+                    </a>
+                    <a
+                        id="ingénierie"
+                        onClick={() => this.onClickMenu('ingénierie')}
+                        className={`${activeItem === 'ingénierie' ? 'menu-item navbar-Selected' : 'menu-item'}`}
+                        href="#ingenierie">Ingénierie
+                    </a>
+                    <a
+                        id="industrialisation"
+                        onClick={() => this.onClickMenu('industrialisation')}
+                        className={`${activeItem === 'industrialisation' ? 'menu-item navbar-Selected' : 'menu-item'}`}
+                        href="#industrialisation">Industrialisation
+                    </a>
+                    <a
+                        id="qualification"
+                        onClick={() => this.onClickMenu('qualification')}
+                        className={`${activeItem === 'qualification' ? 'menu-item navbar-Selected' : 'menu-item'}`}
+                        href="#qualification">Qualification produit
+                    </a>
                     <Dropdown className='NavBarDropdown' href="#/" text='Parent' options={options} simple item />
                 </Menu>
             </div>
