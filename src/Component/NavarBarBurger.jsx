48fb5e6409fbb3e4f2c5f229069a48b9a7a422b0
Romain Roux <cuisinier.sonore@gmail.com>
2019-05-21 16:32:44 +0200

    sous-menu navBar

--- src/Component/NavarBarBurger.jsx
+++ src/Component/NavarBarBurger.jsx
@@ -1,8 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Dropdown } from 'semantic-ui-react'
 import { bubble as Menu } from 'react-burger-menu';
 
 class NavarBarBurger extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            options: [
+                { key: 1, text: 'Child 1', value: 1, href:"#/" },
+                { key: 2, text: 'Child 2', value: 2, href:"#/" },
+                { key: 3, text: 'Child 3', value: 3, href:"#/" },
+            ]
+        }
+    }
     render() {
+        const { options } =this.state;
         return (
             <div >
                 <Menu className='NavBarApp' pageWrapId={"page-wrap"}>
@@ -11,6 +23,7 @@ class NavarBarBurger extends Component {
                     <a id="ingénierie" className="menu-item" href="#ingenierie">Ingénierie</a>
                     <a id="industrialisation" className="menu-item" href="#industrialisation">Industrialisation</a>
                     <a id="qualification" className="menu-item" href="#qualification">Qualification produit</a>
+                    <Dropdown className='NavBarDropdown' href="#/" text='Parent' options={options} simple item />
                 </Menu>
             </div>
         );
