0405b9ef3101e94857562fbfcc5c3995375e50ca
Romain Roux <45852397+Rom1Roux@users.noreply.github.com>
2019-05-21 13:11:38 +0200

    Update Qualification.jsx

--- src/Component/Qualification.jsx
+++ src/Component/Qualification.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Qualification() {
     return (
         <div>
-            <h2>QUALIFICATION PRODUIT</h2>
+            <h2>Qualification produit</h2>
             <ul>
                 <li>Développement des tests et des moyens de qualification </li>
                 <li>Certification auprès des autorités FAA et EASA</li>
